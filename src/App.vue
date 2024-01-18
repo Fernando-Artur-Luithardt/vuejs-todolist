641d46efc26f7f70a48042dab0ca144dcf19eff1
Fernando Artur Luithardt <fluithardt@gmail.com>
2024-01-18 07:01:57 -0300

    v-for, v-bind:key

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <div 
+          v-for="(obj, index) in todos"
+          v-bind:key="obj.id"
+          class="todos-items"
+      >
+          {{ index }} - {{ obj.title }}
+      </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "Estudar vue",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "Estudar react",
+          "completed": false
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
+.todos-items {
+  background: #000;
+  margin: 0 0 5px 0;
+  padding: 3px 5px;
+  color: #fff;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
